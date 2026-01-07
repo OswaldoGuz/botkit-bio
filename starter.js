24f445e9f0284ba666378b688d710625c51591fd
laurent <laurent.parmentier@smart-force.com>
2026-01-07 15:04:22 -0600

    Added CloudWatcher

--- starter.js
+++ starter.js
@@ -1,17 +1,29 @@
 var botId = "st-12345";
 var botName = "testBot";
 var sdk = require("./lib/sdk");
+const AWS = require("aws-sdk");
+
+require("dotenv").config();
+
+// Configure AWS credentials BEFORE initializing CloudWatch Logger
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+    region: process.env.AWS_REGION || "us-east-1",
+});
+
+const logger = new CloudWatchLogger(`/botkit/${botName}`, `stream-${botId}`);
 
 module.exports = {
-    botId   : botId,
-    botName : botName,
+    botId: botId,
+    botName: botName,
 
-    on_user_message : function(requestId, data, callback) {
+    on_user_message: function (requestId, data, callback) {
         if (data.message === "Hi") {
             data.message = "Hello";
             //Sends back 'Hello' to user.
             return sdk.sendUserMessage(data, callback);
-        } else if(!data.agent_transfer){
+        } else if (!data.agent_transfer) {
             //Forward the message to bot
             return sdk.sendBotMessage(data, callback);
         } else {
@@ -19,26 +31,23 @@ module.exports = {
             return sdk.sendUserMessage(data, callback);
         }
     },
-    on_bot_message  : function(requestId, data, callback) {
-        if (data.message === 'hello') {
-            data.message = 'The Bot says hello!';
+    on_bot_message: function (requestId, data, callback) {
+        if (data.message === "hello") {
+            data.message = "The Bot says hello!";
         }
         //Sends back the message to user
-        
+
         return sdk.sendUserMessage(data, callback);
     },
-    on_agent_transfer : function(requestId, data, callback){
+    on_agent_transfer: function (requestId, data, callback) {
         return callback(null, data);
     },
-    on_event : function (requestId, data, callback) {
+    on_event: function (requestId, data, callback) {
         console.log("on_event -->  Event : ", data.event);
         return callback(null, data);
     },
-    on_alert : function (requestId, data, callback) {
+    on_alert: function (requestId, data, callback) {
         console.log("on_alert -->  : ", data, data.message);
         return sdk.sendAlertMessage(data, callback);
-    }
-
+    },
 };
-
-
